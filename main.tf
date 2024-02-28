2fedb296747d029efa07656abe9f7d3431db4d54
Deepthi <dpdevops6@gmail.com>
2024-02-28 18:12:15 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -13,14 +13,15 @@ module "vpc" {
 }
 
 
-#module "alb" {
-#  source   = "git::https://github.com/dpathim/tf-module-alb.git"
-#  for_each = var.alb
-#  internal = each.value["internal"]
-#  lb_type = each.value["lb_type"]
-#  sg_ingress_cidr = each.value["sg_ingress_cidr"]
-#  vpc_id = each.value["internal"] ? lookup(lookup(module.vpc, "main", null ),"vpc_id" , null) : var.default_vpc_id
-#  tags = var.tags
-#  env = var.env
-#  sg_port = each.value["sg_port"]
-#}
+module "alb" {
+  source   = "git::https://github.com/dpathim/tf-module-alb.git"
+  for_each = var.alb
+  internal = each.value["internal"]
+  lb_type = each.value["lb_type"]
+  sg_ingress_cidr = each.value["sg_ingress_cidr"]
+  vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
+  subnets = each.value["internal"] ? data.aws_subnets.subnets.ids : local.app_subnets
+  tags = var.tags
+  env = var.env
+  sg_port = each.value["sg_port"]
+}
