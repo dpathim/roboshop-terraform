31ce2756d9b2904833bc5b28f3e21292425f6ae3
Deepthi <dpdevops6@gmail.com>
2024-02-29 17:16:00 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   source                  = "git::https://github.com/dpathim/tf-module-vpc.git"
   for_each                = var.vpc
   cidr                    = each.value["cidr"]
-#  subnets                 = each.value["subnets"]
+  subnets                 = each.value["subnets"]
 #  default_vpc_id          = var.default_vpc_id
 #  default_vpc_cidr        = var.default_vpc_cidr
 #  default_vpc_route_table_id = var.default_vpc_route_table_id
