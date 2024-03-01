da9924f210a8ffa0a97195dadba36ad48cc291cf
Deepthi <dpdevops6@gmail.com>
2024-03-01 18:37:41 -0500

    Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-#output "vpc" {
-#  value = module.vpc
-#}
+output "vpc" {
+  value = module.vpc
+}
 
 #output "vpc" {
 #  value = local.app_subnets
 #}
 
-output "vpc" {
-  value = data.aws_subnets.subnets.ids
-}
+#output "vpc" {
+#  value = data.aws_subnets.subnets.ids
+#}
 
