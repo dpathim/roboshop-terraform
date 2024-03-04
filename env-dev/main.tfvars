9983e4e643030b05df60f6447e69319da42fb9ca
Deepthi <dpdevops6@gmail.com>
2024-03-04 18:26:20 -0500

    Terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -56,6 +56,7 @@ docdb = {
     backup_retention_period = 5
     preferred_backup_window = "07:00-09:00"
     skip_final_snapshot     = true
+    engine_version          = "4.0.0"
 
 
   }
--- main.tf
+++ main.tf
@@ -37,6 +37,7 @@ module "docdb" {
   skip_final_snapshot        = each.value["skip_final_snapshot"]
  vpc_id                      = local.vpc_id
  sg_ingress_cidr             = local.app_subnets_cidr
+  engine_version             = each.value["engine_version"]
 
 
 }
