59aed8dee88a2c98100321b11f5130cc0e50c507
Deepthi <dpdevops6@gmail.com>
2024-03-09 16:06:31 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -99,40 +99,39 @@ module "rabbitmq" {
   instance_type              = each.value["instance_type"]
 
 }
-#
-#
-#module "app" {
-#  source                   = "git::https://github.com/dpathim/tf-module-app.git"
-#
-#  tags                     = var.tags
-#  env                      = var.env
-#  zone_id                  = var.zone_id
-#  ssh_ingress_cidr         = var.ssh_ingress_cidr
-#  default_vpc_id           = var.default_vpc_id
-#
-#  for_each = var.apps
-#  component = each.key
-#  port      = each.value["port"]
-# instance_type = each.value["instance_type"]
-# desired_capacity = each.value["desired_capacity"]
-# max_size = each.value["max_size"]
-# min_siz = each.value["min_siz"]
-#  lb_priority = each.value["lb_priority"]
-#
-#
-#
-#
-#  sg_ingress_cidr             = local.app_subnets_cidr
-#  vpc_id                      = local.vpc_id
-#  subnet_ids                 = local.app_subnets
-#
-#  private_alb_name                    = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
-#  public_alb_name                    = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
-#  private_listener                    = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
-#  public_listener                    = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
-#
-#
-#}
+
+module "app" {
+  source                   = "git::https://github.com/dpathim/tf-module-app.git"
+
+  tags                     = var.tags
+  env                      = var.env
+  zone_id                  = var.zone_id
+  ssh_ingress_cidr         = var.ssh_ingress_cidr
+  default_vpc_id           = var.default_vpc_id
+
+  for_each = var.apps
+  component = each.key
+  port      = each.value["port"]
+ instance_type = each.value["instance_type"]
+ desired_capacity = each.value["desired_capacity"]
+ max_size = each.value["max_size"]
+ min_siz = each.value["min_siz"]
+  lb_priority = each.value["lb_priority"]
+
+
+
+
+  sg_ingress_cidr             = local.app_subnets_cidr
+  vpc_id                      = local.vpc_id
+  subnet_ids                 = local.app_subnets
+
+  private_alb_name                    = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
+  public_alb_name                    = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+  private_listener                    = lookup(lookup(lookup(module.alb, "private", null), "listener", null), "arn", null)
+  public_listener                    = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+
+
+}
 
 
 
